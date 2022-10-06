baeff5eb382efca3ce6e3527898106fd82a72791
user <user@DESKTOP-2PGRMF4>
2022-10-06 13:56:07 +0900

    백준 merge sort 연습 (22.10.06)

--- 백준연습/src/recursion/No24060.java
+++ 백준연습/src/recursion/No24060.java
@@ -6,11 +6,13 @@ import java.util.StringTokenizer;
 public class No24060 {
 
 	static int cnt, i=0;
+	static int[] tmp;
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] n = new int[Integer.parseInt(st.nextToken().toString())];
+		tmp = new int[n.length];
 		cnt=Integer.parseInt(st.nextToken().toString());
 		
 		st= new StringTokenizer(br.readLine());
@@ -18,9 +20,46 @@ public class No24060 {
 			n[i]=Integer.parseInt(st.nextToken());
 			i++;
 		}
-		i=0;
+		mergeSort(n, 0, n.length-1);
+		if(cnt>0) i=-1;
+		System.out.println(i);
 		
 
+	}
+	
+	static void mergeSort(int[] arr, int l, int r) {
+		 
+		if (l == r) return;
+	    int m = (l + r) / 2;
+	    mergeSort(arr, l, m); 
+	    mergeSort(arr, m + 1, r);
+	    merge(arr, l, m, r);
+	}
+	
+	static void merge(int[] arr, int l, int m, int r){
+		  int L, R, k, a;
+		    L = l;
+		    R = m + 1;
+		    k = l;
+		 
+		    while (L <= m && R <= r)
+		        tmp[k++] = arr[L] <= arr[R] ? arr[L++] : arr[R++];
+		    
+		    
+		    if (L>m) 
+		        for (a = R; a <= r; a++) tmp[k++] = arr[a];
+		    else
+		        for (a = L; a <= m; a++) tmp[k++] = arr[a];
+		     
+		    for (a = l; a <= r; a++) {
+		    	arr[a] = tmp[a];
+		    	cnt--;
+		    	if(cnt==0) {
+		    		i=tmp[a];
+		    		return;
+		    	}
+		    }
+		     
 	}
 
 }
